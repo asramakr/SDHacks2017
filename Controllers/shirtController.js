fd34f795e86ec7fa47d090b74902c5b762ebe6e6
Arun Ramakrishnan <Arun@Aruns-MacBook-Pro-2.local>
2017-10-21 15:14:12 -0700

    Added Dropdown functionaility to sidebar.

--- Controllers/shirtController.js
+++ Controllers/shirtController.js
@@ -1,3 +1,18 @@
 baseWebPageApp.controller('shirtController', function shirtController($scope) {
-  
+  $scope.formalShirts = [];
+  $scope.casualShirts = [];
+  function typeofStyle(){
+    var myStyle = document.getElementById("styleValue");
+    if(myStyle.value == "Formal"){
+
+    }
+    else{
+      $scope.casualShirts = [
+        {img: "/images/crewneckSweater.jpg", name: "Crew Neck Sweater"},
+        {img: "/images/flannel.jpg", name: "Flannel"}, 
+        {img: "/images/LogoCrew.jpg", name: "Logo Crew"},
+        {img: "/images/PocketT.jpg", name: "Pocket T"}
+      ];
+    }
+  }
 });
